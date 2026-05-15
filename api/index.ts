413028a5ea4c47e8e2b4606ae5719767b075ffc4
Mac <mac@Macs-MacBook-Air.local>
2026-05-15 18:23:39 +0700

    fix: gitignore compiled api js files

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,6 @@
-export default async function handler(req: any, res: any) {
-  const mod = await import("../server/_core/index");
-  const { createApp } = mod;
+import { createApp } from "../server/_core/index";
 
+export default async function handler(req: any, res: any) {
   const { app } = await createApp();
 
   return app(req, res);
